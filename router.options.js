677360c94d8d43cccd06f2d475cbfcc224bb1355
Ignacio Chalub <ignaciochalub@Ignacios-MacBook-Pro.local>
2024-05-02 09:41:34 +0200

    change

--- router.options.js
+++ router.options.js
@@ -1,5 +1,6 @@
 export default {
-    base: '/nuxt-test/', // Set the base path according to your GitHub Pages repository name
-    // You can specify other router options here as needed
-  };
-  
+    target: 'static',
+    router: {
+      base: '/<repository-name>/'
+    }
+}
